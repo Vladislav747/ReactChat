67665ad8b5eaa5d2d9251792d8dc80a4ab64d533
Vladislav <zippo585@gmail.com>
2020-06-11 00:20:44 +0500

    Updates

---
+++ precache-manifest.2430eb5ed196f177f9eb73e076cc8aa2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f26d41a5e75c7bcf86e6e8b4f57123c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c9e4a1c109a3ab7b5f9",
+    "url": "/static/css/2.03087420.chunk.css"
+  },
+  {
+    "revision": "bfbc9c97c41dac209b24",
+    "url": "/static/css/main.7b7255ce.chunk.css"
+  },
+  {
+    "revision": "9c9e4a1c109a3ab7b5f9",
+    "url": "/static/js/2.6a522812.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.6a522812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbc9c97c41dac209b24",
+    "url": "/static/js/main.09277b10.chunk.js"
+  },
+  {
+    "revision": "410746fb762aedca816a",
+    "url": "/static/js/runtime-main.775ebe7c.js"
+  },
+  {
+    "revision": "1456f01338efe100733984d48807dd08",
+    "url": "/static/media/noreaded.1456f013.svg"
+  },
+  {
+    "revision": "2b64c3370f831c12c2ac702c03eb3c00",
+    "url": "/static/media/readed.2b64c337.svg"
+  }
+]);
